be0be0899d7cf05d291a62671f06cda977c03ebd
yaman1337 <yaman.sarabariya@gmail.com>
2023-05-24 11:05:43 +0545

    [refactor] add formatter for scraped results

--- scraper/topShare.js
+++ scraper/topShare.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import getTextFromHtml from "../utils/getTextFromHtml.js";
 
 const url =
   "https://www.sharesansar.com/top-tradedshares?draw=1&columns%5B0%5D%5Bdata%5D=DT_Row_Index&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=symbol&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=false&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=companyname&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=false&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=traded_quantity&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=false&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=close&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=false&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=50&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1683474111302";
@@ -13,6 +14,13 @@ export default async function getTopGainer(url) {
     });
 
     const rawData = await res.json();
+
+    for (let i = 0; i < rawData.data.length; i++) {
+      let formattedCompanyname = getTextFromHtml(rawData.data[i].companyname);
+      let formattedSymbol = getTextFromHtml(rawData.data[i].symbol);
+      rawData.data[i].companyname = formattedCompanyname;
+      rawData.data[i].symbol = formattedSymbol;
+    }
 
     return rawData;
   } catch (error) {
