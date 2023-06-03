e0645d07c9b7301c19ab1730610b5bdcda0b5f0f
yaman1337 <yaman.sarabariya@gmail.com>
2023-06-03 14:43:33 +0545

    [refactor]: add contact number of top broker in top broker response

---
+++ scraper/brokerDetails.js
@@ -0,0 +1,37 @@
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
+
+export default async function getBrokerDetails() {
+  try {
+    const res = await fetch("https://merolagani.com/BrokerList.aspx");
+    const htmlData = await res.text();
+
+    const { document } = new JSDOM(htmlData).window;
+
+    const tableElement = document.querySelector(
+      "#ctl00_ContentPlaceHolder1_divData > div > table"
+    );
+
+    const borderCodeArray = Array.from(
+      tableElement.querySelectorAll("tbody > tr > td:nth-child(1)")
+    ).map((item) => item.textContent.trim());
+
+    const phoneNumberArray = Array.from(
+      tableElement.querySelectorAll("tbody > tr > td:nth-child(3)")
+    ).map((item) =>
+      item.textContent.trim().split(",").length >= 2
+        ? item.textContent.trim().split(",")[0]
+        : item.textContent.trim()
+    );
+
+    let finalData = {};
+
+    for (let i = 0; i < borderCodeArray.length; i++) {
+      finalData[borderCodeArray[i]] = phoneNumberArray[i];
+    }
+
+    return finalData;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- scraper/topBroker.js
+++ scraper/topBroker.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import getBrokerDetails from "./brokerDetails.js";
 
 const url = `https://www.sharesansar.com/top-brokers?draw=1&columns%5B0%5D%5Bdata%5D=DT_Row_Index&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=number&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=name&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=buyerAmount&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=false&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=sellerAmount&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=false&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=totalAmount&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=false&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=matchingAmount&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=-1&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1683363339865`;
 
@@ -6,12 +7,18 @@ const url = `https://www.sharesansar.com/top-brokers?draw=1&columns%5B0%5D%5Bdat
 
 export default async function topBroker(url) {
   try {
+    const brokerDetails = await getBrokerDetails();
+
     const res = await fetch(url, {
       headers: {
         "x-requested-with": "XMLHttpRequest",
       },
     });
     const data = await res.json();
+
+    data.data.map((item) => {
+      item["contact_number"] = brokerDetails[item["number"]];
+    });
 
     return data;
   } catch (error) {
